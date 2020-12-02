5c052a9f43706361b7dec6efc9af22e3c57d781c
bloges56 <brady.drew.logan@gmail.com>
2020-12-02 09:47:48 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamSixHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
