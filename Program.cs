30d975daccfdf863987cfd99a4c4b81f5a180ffd
bloges56 <brady.drew.logan@gmail.com>
2020-12-02 10:39:15 -0600

    created rolodex

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TeamSixHeist
 {
@@ -6,7 +7,45 @@ namespace TeamSixHeist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IRobber> rolodex = new List<IRobber>()
+            {
+                new Hacker()
+                {
+                    Name = "Bill Burr",
+                    SkillLevel = 30,
+                    PercentageCut = 25
+                },
+                new Hacker()
+                {
+                    Name = "Sam",
+                    SkillLevel = 75,
+                    PercentageCut = 40
+                },
+                new LockSpecialist()
+                {
+                    Name = "CJ",
+                    SkillLevel = 70,
+                    PercentageCut = 40
+                },
+                new LockSpecialist()
+                {
+                    Name = "Tom Segura",
+                    SkillLevel = 60,
+                    PercentageCut = 30
+                },
+                new Muscle()
+                {
+                    Name = "Brady",
+                    SkillLevel = 88,
+                    PercentageCut = 15
+                },
+                new Muscle()
+                {
+                    Name = "John Mulaney",
+                    SkillLevel = 20,
+                    PercentageCut = 45
+                }
+            };
         }
     }
 }
