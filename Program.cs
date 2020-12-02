cda2dfc7cff79bd1a0f08f111adce7114872db85
bloges56 <brady.drew.logan@gmail.com>
2020-12-02 11:01:35 -0600

    prompt user for name and specialty

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace TeamSixHeist
     {
         static void Main(string[] args)
         {
+            //create an initial list of robbers
             List<IRobber> rolodex = new List<IRobber>()
             {
                 new Hacker()
@@ -46,6 +47,58 @@ namespace TeamSixHeist
                     PercentageCut = 45
                 }
             };
+
+            //print initial number of criminals
+            Console.WriteLine($"Number of criminals: {rolodex.Count}");
+
+            //allow user to add as many criminals as they want
+            while(true)
+            {
+                //prompt for a new criminal
+                Console.WriteLine("Is there another criminal you know? If so, what is their name?");
+                string name = Console.ReadLine();
+
+                if(name == "")
+                {
+                    break;
+                }
+
+                int selection = 0;
+                //prompt for a specialty
+                while(true)
+                {
+                    Console.WriteLine("What is their specialty?\n1) Hacker\n2) Muscle\n3) Lock Specialist");
+                    try
+                    {
+                        selection = Int32.Parse(Console.ReadLine());
+                        if(selection <= 0 || selection > 3)
+                        {
+                            throw new Exception();
+                        }
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please enter either 1, 2, or 3");
+                    }
+                }
+
+                //declare a type of instance based on the user's choice
+                IRobber newCriminal;
+                switch (selection)
+                {
+                    case 1:
+                        newCriminal = new Hacker();
+                        break;
+                     case 2:
+                        newCriminal = new Muscle();
+                        break;
+                    case 3:
+                        newCriminal = new LockSpecialist();
+                        break;    
+                }
+
+            }
         }
     }
 }
